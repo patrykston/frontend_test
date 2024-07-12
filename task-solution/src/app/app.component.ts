33ddcac40eb9633b0bdac58dc155dfccd5a8d554
patrykston <stonpatryk@gmail.com>
2024-07-12 13:11:54 +0200

    Added routing and Home component

--- task-solution/src/app/app.component.ts
+++ task-solution/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- task-solution/src/app/header/header.component.ts
+++ task-solution/src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderIconComponent } from '../header-icon/header-icon.component';
 import { HeaderTextComponent } from '../header-text/header-text.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderIconComponent, HeaderTextComponent],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, HeaderIconComponent, HeaderTextComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
